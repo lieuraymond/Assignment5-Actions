503ba9615db109b1908ddfc47f2f8fa1f668311f
lieuraymond <lieu.raymond@gmail.com>
2020-03-01 22:25:28 -0800

    Area of Circle

--- task.py
+++ task.py
@@ -1,2 +1,7 @@
+import math
+
 def firstrun():
     return "success"
+
+def area_circle(radius):
+    return pi * math.pow(radius, 2)
--- tests.py
+++ tests.py
@@ -11,3 +11,7 @@ class TestCase(unittest.TestCase):
     def test2(self):
         expected = "failure"
         self.assertNotEqual(expected, task.firstrun())
+
+    def test_area_circle(self):
+        radius = 5
+        self.assertEqual(area_circle(5), pi * pow(radius, 2))
